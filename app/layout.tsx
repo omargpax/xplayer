45fc7c7c5bc64a45234e0e9c7bb99d9216471d92
omargpax <omargpax.dev@gmail.com>
2026-03-28 20:19:02 -0500

    feat: SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,51 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Footer from "../components/footer"
 
-export const metadata: Metadata = {
-  title: 'Xplay',
-  description: 'Created with v0+vercel',
-  generator: 'v0.dev',
-}
+export const metadata = {
+  metadataBase: new URL('https://xplay.omargpax.dev'),
+  manifest: '/manifest.json',
+  title: " Xplay",
+  description: "Xplay is a music player built with React,Tailwind CSS and Vite. It features a sleek and modern design, with a focus on simplicity and ease of use. With Xplay, you can easily manage your music library, create playlists, and enjoy your favorite tunes in style.",
+  author: "Omar A. Guerrero",
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/logo.png',
+  },
+  openGraph: {
+    title: "Xplay - Music Player",
+    description: "Xplay a minimalist music player built with React and Tailwind CSS. It features a sleek and modern design, with a focus on simplicity and ease of use.",
+    url: "https://xplay.omargpax.dev",
+    siteName: "Xplay",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Xplay - Music Player Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Xplay - Music Player",
+    description: "Check out Xplay, a minimalist music player built with React and Tailwind CSS.",
+    creator: "@omargpax",
+    images: ["/preview.png"],
+  },
+  keywords: [
+    "Xplay",
+    "Music Player",
+    "React",
+    "Tailwind CSS",
+    "Vite",
+    "Minimalist Design",
+    "Playlist Management",
+    "Audio Player",
+    "Web Music Player"
+  ].join(', '),
+};
 
 export default function RootLayout({
   children,
